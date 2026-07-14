362407cd3dc8ebbaa3e011c778ed2349f5242b16
Krish Verma <kishuverma2005@gmail.com>
2026-07-14 20:57:15 +0530

    Time: 42 ms (24.56%), Space: 55.8 MB (48.72%) - LeetHub

--- 0391-perfect-rectangle/0391-perfect-rectangle.java
+++ 0391-perfect-rectangle/0391-perfect-rectangle.java
@@ -14,10 +14,9 @@ class Solution {
       x2 = Math.max(x2, r[2]);
       y2 = Math.max(y2, r[3]);
 
-      // the four points of the current rectangle
-      String[] points = new String[] {r[0] + " " + r[1], //
-                                      r[0] + " " + r[3], //
-                                      r[2] + " " + r[1], //
+      String[] points = new String[] {r[0] + " " + r[1], 
+                                      r[0] + " " + r[3], 
+                                      r[2] + " " + r[1], 
                                       r[2] + " " + r[3]};
       for (final String point : points)
         if (!corners.add(point))
@@ -26,9 +25,9 @@ class Solution {
 
     if (corners.size() != 4)
       return false;
-    if (!corners.contains(x1 + " " + y1) || //
-        !corners.contains(x1 + " " + y2) || //
-        !corners.contains(x2 + " " + y1) || //
+    if (!corners.contains(x1 + " " + y1) || 
+        !corners.contains(x1 + " " + y2) || 
+        !corners.contains(x2 + " " + y1) || 
         !corners.contains(x2 + " " + y2))
       return false;
     return area == (x2 - x1) * (y2 - y1);
